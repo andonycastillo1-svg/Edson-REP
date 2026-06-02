efcd57bc6be8dffc7ea846aa2705bee3b6fda10b
alexandertinti210-del <alexandertinti210@gmail.com>
2026-06-02 07:43:42 -0600

    vehiculo y bodega

--- app/Models/Bodega.php
+++ app/Models/Bodega.php
@@ -1,31 +1,31 @@
-<?php
+    <?php
 
-namespace App\Models;
+    namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
+    use Illuminate\Database\Eloquent\Model;
 
-class Bodega extends Model
-{
-    protected $table = 'bodegas';
-
-    protected $fillable = [
-        'nombre',
-        'ubicacion',
-        'tipo',
-    ];
-
-    public function inventarios()
+    class Bodega extends Model
     {
-        return $this->hasMany(\App\Models\Inventario::class, 'bodega_id');
-    }
+        protected $table = 'bodegas';
 
-    public function movimientosOrigen()
-    {
-        return $this->hasMany(\App\Models\Movimiento::class, 'bodega_origen_id');
-    }
+        protected $fillable = [
+            'nombre',
+            'ubicacion',
+            'tipo',
+        ];
 
-    public function movimientosDestino()
-    {
-        return $this->hasMany(\App\Models\Movimiento::class, 'bodega_destino_id');
+        public function inventarios()
+        {
+            return $this->hasMany(\App\Models\Inventario::class, 'bodega_id');
+        }
+
+        public function movimientosOrigen()
+        {
+            return $this->hasMany(\App\Models\Movimiento::class, 'bodega_origen_id');
+        }
+
+        public function movimientosDestino()
+        {
+            return $this->hasMany(\App\Models\Movimiento::class, 'bodega_destino_id');
+        }
     }
-}
